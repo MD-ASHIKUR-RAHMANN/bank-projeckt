5fe3110da5aaf93e0148fce90f70ef71071dd6c0
aashik111 <abduljalilpbn@gmail.com>
2023-09-14 11:20:47 +0600

    done-normaly

--- function.js
+++ function.js
@@ -20,3 +20,11 @@ function valueSet(age){
 var fst = valueSet(20);
 var scnd = valueSet(5);
 console.log(scnd)
+//
+// function permiter use korle sei function kotaw teke call korle ekta variable a rakte hobe noile output asbena and function er vetor ki ghotbe seta function vetor diclear korte hobe bayre teke korle hobena
+
+function add(one, two){
+    return one + two
+}
+var result = add(15, 20);
+console.log(result);
